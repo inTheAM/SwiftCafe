23ed4f667f37ef68238515d1f111416afa3443f3
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 21:56:00 +0300

    Add readOffset extension on View

--- SwiftCafe/Extensions/View.swift
+++ SwiftCafe/Extensions/View.swift
@@ -11,4 +11,14 @@ extension View    {
     func overlayDivider()    ->    some View    {
         return self.overlay(Divider(),    alignment: .bottom)
     }
+    
+    func readOffset(_ offsetHandler: @escaping (_ rect: CGRect) -> ()) -> some View {
+        GeometryReader { geometry in
+            self
+                .preference(key: OffsetKey.self, value: geometry.frame(in: .named("ScrollView")))
+        }
+        .onPreferenceChange(OffsetKey.self) { rect in
+            offsetHandler(rect)
+        }
+    }
 }
