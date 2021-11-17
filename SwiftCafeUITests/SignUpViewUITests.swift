357256b892ee027c9b066c0a742e0bf0f2518079
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 15:58:28 +0300

    Add testEmailTextFieldExists to SignUpViewUITests

--- SwiftCafeUITests/SignUpViewUITests.swift
+++ SwiftCafeUITests/SignUpViewUITests.swift
@@ -10,10 +10,18 @@ import XCTest
 final class SignUpViewUITests: XCTestCase {
     let app = XCUIApplication()
     
+//    MARK: - Setup
     override func setUpWithError() throws {
         app.launch()
     }
     
+//    MARK: - Tests
+    func testEmailTextFieldExists() throws {
+        let email = app.textFields["email"]
+        XCTAssert(email.exists)
+    }
+    
+//    MARK: - Teardown
     override func tearDownWithError() throws {
         app.terminate()
     }
