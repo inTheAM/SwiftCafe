3caca1dd26fae95d53478fdb4d6dc743d6ff9a3f
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 12:24:23 +0300

    Add sample User

---
+++ SwiftCafeTests/Extensions/User+Sample.swift
@@ -0,0 +1,12 @@
+//
+//  User+Sample.swift
+//  SwiftCafeTests
+//
+//  Created by Ahmed Mgua on 17/11/2021.
+//
+
+@testable import SwiftCafe
+
+extension User {
+    static let sample = User.CreateData(email: "testuser@test.com", password: "password@123")
+}
