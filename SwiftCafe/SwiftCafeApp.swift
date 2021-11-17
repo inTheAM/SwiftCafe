3bd0067ef68730f445f3bbe642c16afcdce32156
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 11:42:17 +0300

    Initial Commit

---
+++ SwiftCafe/SwiftCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCafeApp.swift
+//  SwiftCafe
+//
+//  Created by Ahmed Mgua on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
