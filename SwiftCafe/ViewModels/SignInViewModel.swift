82494f7ddda88bbe177f25a8d0df337720e501db
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 13:43:55 +0300

    Add Form Validation to SignInViewModel

--- SwiftCafe/ViewModels/SignInViewModel.swift
+++ SwiftCafe/ViewModels/SignInViewModel.swift
@@ -10,10 +10,79 @@ import Foundation
 
 final class SignInViewModel: ObservableObject {
     private let authService: AuthServiceProtocol
+    private var cancellables = Set<AnyCancellable>()
     
+//    MARK: - Inputs
+    @Published var email = ""
+    @Published var password = ""
+    
+//    MARK: - Validation
+    @Published var isFormValid = false
+    
+//    MARK: - Inline Errors
+    @Published var signInErrorDescription = ""
     
 //    MARK: - Initializer
     init(authService: AuthServiceProtocol = AuthService.shared) {
         self.authService = authService
+        
+        isFormValidPublisher
+            .receive(on: RunLoop.main)
+            .map { formStatus in
+                switch formStatus {
+                case .valid:
+                    self.signInErrorDescription = ""
+                    return true
+                default:
+                    self.signInErrorDescription = formStatus.rawValue
+                    return false
+                }
+            }
+            .assign(to: \.isFormValid, on: self)
+            .store(in: &cancellables)
+    }
+}
+
+//  MARK: - Form Validation
+enum FormStatus: String {
+    case emailEmpty = "Input your email address",
+         passwordEmpty = "Enter your password",
+         valid = ""
+}
+
+extension SignInViewModel {
+    private var isEmailEmptyPublisher: AnyPublisher<Bool, Never> {
+        $email
+            .dropFirst()
+            .debounce(for: 0.2, scheduler: RunLoop.main)
+            .map {
+                $0.isEmpty
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    private var isPasswordEmptyPublisher: AnyPublisher<Bool, Never> {
+        $password
+            .dropFirst()
+            .debounce(for: 0.2, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .map { password in
+                password.isEmpty
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    private var isFormValidPublisher: AnyPublisher<FormStatus, Never> {
+        Publishers.CombineLatest(isEmailEmptyPublisher, isPasswordEmptyPublisher)
+            .map { emailEmpty, passwordEmpty in
+                if emailEmpty {
+                    return .emailEmpty
+                }
+                if passwordEmpty {
+                    return .passwordEmpty
+                }
+                return .valid
+            }
+            .eraseToAnyPublisher()
     }
 }
