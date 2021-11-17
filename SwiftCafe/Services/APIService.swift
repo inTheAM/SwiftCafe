6cd67eae6bb3b012df199fb0eb7490c47028ce77
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 12:22:04 +0300

    Add APIService

---
+++ SwiftCafe/Services/APIService.swift
@@ -0,0 +1,13 @@
+//
+//  APIService.swift
+//  SwiftCafe
+//
+//  Created by Ahmed Mgua on 17/11/2021.
+//
+
+import Foundation
+
+struct APIService    {
+    static let baseURL  =   URL(string: "http://127.0.0.1:8090/api/")
+    
+}
