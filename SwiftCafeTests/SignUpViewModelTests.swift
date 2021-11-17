f80a132e5be2d5bbf8ef5174843e5be9fb9ee9c4
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 11:46:34 +0300

    Add SignUpViewModelTests & project folders

---
+++ SwiftCafeTests/SignUpViewModelTests.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftCafeTests.swift
+//  SwiftCafeTests
+//
+//  Created by Ahmed Mgua on 17/11/2021.
+//
+
+import XCTest
+@testable import SwiftCafe
+
+class SignUpViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+}
