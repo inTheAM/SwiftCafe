603a60a7be4e8afee2935a39707bdd50f4b6357a
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 12:39:52 +0300

    Add XCTestCase+Await extension on Combine Publishers

---
+++ SwiftCafeTests/Extensions/XCTestCase+Await.swift
@@ -0,0 +1,39 @@
+//
+//  XCTestCase+Await.swift
+//  SwiftCafeTests
+//
+//  Created by Ahmed Mgua on 17/11/2021.
+//
+
+import Combine
+import XCTest
+
+extension XCTestCase {
+    func awaitResult<T: Publisher>(
+        from publisher: T, timeout: TimeInterval = 10, file: StaticString = #file, line: UInt = #line
+    ) throws -> T.Output {
+        var result: Result<T.Output, Error>?
+        let expectation = self.expectation(description: "Awaiting publisher")
+
+        let cancellable = publisher
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    result = .failure(error)
+                case .finished:
+                    break
+                }
+                expectation.fulfill()
+            },
+            receiveValue: { value in
+                result = .success(value)
+            }
+        )
+        waitForExpectations(timeout: timeout)
+        cancellable.cancel()
+        
+        let unwrappedResult = try XCTUnwrap(result, "Awaited publisher did not produce any output", file: file, line: line)
+
+        return try unwrappedResult.get()
+    }
+}
