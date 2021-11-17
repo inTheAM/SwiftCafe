05895fd135881cf358127c387d6f7d53ead7572b
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 12:25:43 +0300

    Add checkEmailAvailability functionality in MockAuthService

--- SwiftCafeTests/MockServices/MockAuthService.swift
+++ SwiftCafeTests/MockServices/MockAuthService.swift
@@ -8,5 +8,15 @@
 @testable import SwiftCafe
 
 final class MockAuthService: AuthServiceProtocol {
+    static var token: String?
+    
+    func checkEmailAvailability(email: String, completion: @escaping (Bool) -> Void) {
+        if email == User.sample.email {
+            completion(false)
+        } else {
+            completion(true)
+        }
+    }
+    
     
 }
