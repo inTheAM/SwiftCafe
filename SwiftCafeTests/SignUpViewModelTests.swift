a29263d31b8b524b3d9442fce9adef8713ab3703
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 11:54:24 +0300

    Add testEmailValidationWithValidEmail

--- SwiftCafeTests/SignUpViewModelTests.swift
+++ SwiftCafeTests/SignUpViewModelTests.swift
@@ -10,9 +10,21 @@ import XCTest
 
 final class SignUpViewModelTests: XCTestCase {
     var viewModel: SignUpViewModel!
+    
     override func setUpWithError() throws {
         let service = MockAuthService()
         viewModel = SignUpViewModel(authService: service)
+    }
+    
+//    MARK: - Email Validation Tests
+    func testEmailValidationWithValidEmail() throws {
+        let isEmailValidPublisher = viewModel.$isEmailValid
+            .dropFirst()
+            .first()
+        
+        viewModel.email = "testuser1@test.com"
+        let isEmailValid = try awaitResult(from: isEmailValidPublisher)
+        XCTAssertTrue(isEmailValid)
     }
 
     override func tearDownWithError() throws {
