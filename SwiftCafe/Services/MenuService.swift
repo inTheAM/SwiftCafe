448b9ae5988e4d3ca62261687466d3c3c79726a9
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 22:44:08 +0300

    Add Debug logic to MenuService

--- SwiftCafe/Services/MenuService.swift
+++ SwiftCafe/Services/MenuService.swift
@@ -15,6 +15,12 @@ struct MenuService: MenuServiceProtocol {
     private let path = "menu"
     
     func fetchMenu(completion: @escaping (Result<[MenuSection], RequestError>) -> Void) {
+        #warning("FIND A WAY TO EXTRACT DEBUG LOGIC")
+        #if DEBUG
+        let sections = [MenuSection(id: UUID(), name: "Section 1", items: []), MenuSection(id: UUID(), name: "Section 2", items: [])]
+        completion(.success(sections))
+        return
+        #else
         NetWorkManager.makeGetRequest([MenuSection].self, path: path + "/sections", authType: .none) { result in
             switch result {
             case .success(let sections):
@@ -24,5 +30,6 @@ struct MenuService: MenuServiceProtocol {
                 completion(.failure(error))
             }
         }
+        #endif
     }
 }
