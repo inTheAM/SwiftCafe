da80af5427c3b53e6793e9ccb075b39ac1499370
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 12:39:26 +0300

    Add checkEmailAvailability in AuthService

--- SwiftCafe/Services/AuthService.swift
+++ SwiftCafe/Services/AuthService.swift
@@ -8,7 +8,13 @@
 import Foundation
 
 protocol AuthServiceProtocol {
-    
+    static var token: String? { get set }
+    func checkEmailAvailability(email: String, completion: @escaping (Bool) -> Void)
+}
+
+enum AuthResult {
+    case success,
+         failure
 }
 
 final class AuthService {
@@ -18,5 +24,17 @@ final class AuthService {
 }
 
 extension AuthService: AuthServiceProtocol {
+    static var token: String? = nil
     
+    func checkEmailAvailability(email: String, completion: @escaping (Bool) -> Void) {
+        let validationData = User.ValidateData(email: email)
+        NetWorkManager.makePostRequestWithoutReturn(sending: validationData, path: "users/validate", authType: .none) { result in
+            switch result {
+            case .success:
+                completion(true)
+            case .failure:
+                completion(false)
+            }
+        }
+    }
 }
