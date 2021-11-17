aa5ecac7ea23e3a00c2ded1b300082dd2e7d8966
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 13:16:32 +0300

    Add signUp functionality to AuthService

--- SwiftCafe/Services/AuthService.swift
+++ SwiftCafe/Services/AuthService.swift
@@ -10,6 +10,7 @@ import Foundation
 protocol AuthServiceProtocol {
     static var token: String? { get set }
     func checkEmailAvailability(email: String, completion: @escaping (Bool) -> Void)
+    func signUp(email: String, password: String, completion: @escaping (AuthResult) -> Void)
 }
 
 enum AuthResult {
@@ -34,6 +35,21 @@ extension AuthService: AuthServiceProtocol {
                 completion(true)
             case .failure:
                 completion(false)
+            }
+        }
+    }
+    
+    func signUp(email: String, password: String, completion: @escaping (AuthResult) -> Void) {
+        let user = User.CreateData(email: email, password: password)
+        
+        NetWorkManager.makePostRequestWithReturn(sending: user, receiving: Token.self, path: "users/signup", authType: .none) { result in
+            switch result {
+            case .success(let token):
+                Self.token = token.value
+                completion(.success)
+                
+            case .failure:
+                completion(.failure)
             }
         }
     }
