d00d6c06c3cb90489aaa52849ee6652383deefa9
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 11:50:00 +0300

    Add setup & teardown in SignUpViewModelTests

--- SwiftCafeTests/SignUpViewModelTests.swift
+++ SwiftCafeTests/SignUpViewModelTests.swift
@@ -9,13 +9,14 @@ import XCTest
 @testable import SwiftCafe
 
 class SignUpViewModelTests: XCTestCase {
-
+    var viewModel: SignUpViewModel!
     override func setUpWithError() throws {
-        
+        let service = MockAuthService()
+        viewModel = SignUpViewModel(authService: service)
     }
 
     override func tearDownWithError() throws {
-        
+        viewModel = nil
     }
 
 }
