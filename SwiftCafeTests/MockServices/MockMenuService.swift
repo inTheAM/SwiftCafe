714676e31742fc4f9098b319284c9e687d09a337
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-17 21:33:06 +0300

    Add fetchMenu to MockMenuService

--- SwiftCafeTests/MockServices/MockMenuService.swift
+++ SwiftCafeTests/MockServices/MockMenuService.swift
@@ -5,8 +5,11 @@
 //  Created by Ahmed Mgua on 17/11/2021.
 //
 
+import Foundation
 @testable import SwiftCafe
 
 struct MockMenuService: MenuServiceProtocol {
-    
+    func fetchMenu(for outlet: UUID, completion: @escaping (Result<[MenuSection], RequestError>) -> Void) {
+        completion(.success(MenuSection.samples))
+    }
 }
