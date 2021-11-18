db2cbb2cbcea86d6a4d83c75a625b85bfdbb2d42
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-18 10:32:40 +0300

    Fix layout and filled in section views

--- SwiftCafe/Views/MenuView.swift
+++ SwiftCafe/Views/MenuView.swift
@@ -11,19 +11,28 @@ struct MenuView: View {
     @StateObject var viewModel = MenuViewModel()
     @State private var isFlashing = false
     
-    @ViewBuilder func flash (_ section: String) -> some View {
-        if isFlashing && section == viewModel.activeSection {
-            Color.primary.opacity(0.1)
-        }
-    }
-    
     var body: some View {
         ScrollViewReader { pageProxy in
-            ScrollView(showsIndicators: false) {
-                LazyVStack(spacing: 0, pinnedViews: [.sectionHeaders]) {
-                    Section(header: MenuSectionsSelector(viewModel: viewModel, pageProxy: pageProxy))    {
+            VStack(spacing: 0) {
+                MenuSectionsSelector(viewModel: viewModel, pageProxy: pageProxy)
+                ScrollView(showsIndicators: false) {
+                    LazyVStack(spacing: 0, pinnedViews: [.sectionHeaders]) {
                         ForEach(viewModel.sections) { section in
-                            //                      Section Views
+                            Section(header: HeaderView(title: section.name)) {
+                                VStack {
+                                    ForEach(section.items)    { food in
+                                        FoodCardView(food: food)
+                                    }
+                                }
+                                .padding()
+                                .overlay(flash(section.name))
+                                
+                            }
+                            .id(section.name)
+                            .overlayDivider()
+                            .readOffset { rect in
+                                activateSection(section, in: rect)
+                            }
                         }
                         
                     }
@@ -31,7 +40,20 @@ struct MenuView: View {
                         viewModel.fetchMenu()
                     }
                 }
+                
             }
+        }
+    }
+    
+    @ViewBuilder private func flash (_ section: String) -> some View {
+        if isFlashing && section == viewModel.activeSection {
+            Color.primary.opacity(0.1)
+        }
+    }
+    
+    private func activateSection(_ section: MenuSection, in rect: CGRect) {
+        if rect.minY < 360 && rect.maxY > 520 && section.name != viewModel.activeSection {
+            viewModel.activeSection = section.name
         }
     }
 }
