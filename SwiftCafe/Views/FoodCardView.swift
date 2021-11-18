05a07012aa240009936c04957f9d3e4af918f9ed
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-18 10:32:20 +0300

    Add FoodCardView

---
+++ SwiftCafe/Views/FoodCardView.swift
@@ -0,0 +1,47 @@
+//
+//  FoodCardView.swift
+//  SwiftCafe
+//
+//  Created by Ahmed Mgua on 18/11/2021.
+//
+
+import SwiftUI
+
+struct FoodCardView: View {
+    let food: Food
+    @State private var showDetails    =    false
+    var body: some View {
+        Button(action:    {
+            showDetails    =    true
+        }) {
+            HStack(alignment: .top, spacing:    0)    {
+                VStack(alignment: .leading) {
+                    Text(food.name)
+                        .font(.caption)
+                        .bold()
+                    
+                    Text(food.details)
+                        .font(.caption2)
+                    
+                    Spacer(minLength: 0)
+                    HStack {
+                        Spacer()
+                        
+                        Text("\(food.price)")
+                            .foregroundColor(.secondary)
+                            .strikethrough()
+                        
+                    }
+                    
+                }.padding(10)
+            }
+            .frame(maxHeight: 80)
+            .cardify()
+            .accessibilityIdentifier(food.name)
+            .fullScreenCover(isPresented: $showDetails)    {
+                Text("Food details")
+            }
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
