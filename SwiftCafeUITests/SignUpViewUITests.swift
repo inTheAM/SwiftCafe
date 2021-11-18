5d38c77a37dc96566a4ec9a98c500b112f6624f8
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-18 10:52:34 +0300

    Add launch environment in SignUpViewUITests

--- SwiftCafeUITests/SignUpViewUITests.swift
+++ SwiftCafeUITests/SignUpViewUITests.swift
@@ -14,6 +14,7 @@ final class SignUpViewUITests: XCTestCase {
     override func setUpWithError() throws {
         app.launch()
         let signUpLink = app.buttons["go to signup"]
+        app.launchEnvironment = ["ENV": "TEST"]
         signUpLink.tap()
     }
     
