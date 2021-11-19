515b78203fdc9dd567c5270d0ac1cc0b95df0b5d
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-19 11:08:34 +0300

    Add CartTests

---
+++ SwiftCafeTests/Tests/CartTests.swift
@@ -0,0 +1,19 @@
+//
+//  CartTests.swift
+//  SwiftCafe
+//
+//  Created by Ahmed Mgua on 19/11/2021.
+//
+
+@testable import SwiftCafe
+import XCTest
+
+final class CartTests: XCTestCase {
+    private var cart: Cart!
+    
+//    MARK: - Setup
+    override func setUpWithError() throws {
+        let cartService = MockCartService()
+        cart = Cart(cartService: cartService)
+    }
+}
