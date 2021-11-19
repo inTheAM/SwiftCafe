9d1c4bbfab7a768618501e0da9b9fa0957aaf8d4
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-19 12:16:31 +0300

    Add CartViewUITests

---
+++ SwiftCafeUITests/Tests/CartViewUITests.swift
@@ -0,0 +1,27 @@
+//
+//  CartViewUITests.swift
+//  SwiftCafeUITests
+//
+//  Created by Ahmed Mgua on 19/11/2021.
+//
+
+@testable import SwiftCafe
+import XCTest
+
+final class CartViewUITests: XCTestCase {
+    let app = XCUIApplication()
+
+// MARK: - Setup
+    override func setUpWithError() throws {
+        app.launchWithTestEnvironment()
+        let cartButton = app.buttons["Cart"]
+        cartButton.tap()
+    }
+
+// MARK: - Tests
+    
+// MARK: - Teardown
+    override func tearDownWithError() throws {
+        app.terminate()
+    }
+}
