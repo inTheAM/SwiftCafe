56ff5f5370b8a08c0356ebad3889464606b477cf
Ahmed Mgua <mguaahmed@gmail.com>
2021-11-19 11:16:10 +0300

    Add testAddingItemToCart

--- SwiftCafeTests/Tests/CartTests.swift
+++ SwiftCafeTests/Tests/CartTests.swift
@@ -17,6 +17,20 @@ final class CartTests: XCTestCase {
         cart = Cart(cartService: cartService)
     }
 
+//    MARK: - Tests
+    func testAddingItemToCart() throws {
+        let contentsPublisher = cart.$contents
+            .first()
+        let food = MenuSection.samples[0].items[0]
+        let quantity = 3
+        cart.addToCart(food, quantity: quantity)
+        
+        let contents = try awaitResult(from: contentsPublisher)
+        XCTAssertEqual(contents[0].food.id, food.id)
+        XCTAssertEqual(contents[0].quantity, quantity)
+    }
+    
+    
 // MARK: - Teardown
     override func tearDownWithError() throws {
         cart = nil
