f546d7d01d9eaded4a6485ab8b207f851e13d809
Ahmed Mgua <mguaahmed@gmail.com>
2021-12-03 10:24:59 +0300

    Add OptionGroup+Samples extension

---
+++ SwiftCafeTests/Extensions/OptionGroup+Samples.swift
@@ -0,0 +1,23 @@
+//
+//  OptionGroup+Samples.swift
+//  SwiftCafeTests
+//
+//  Created by Ahmed Mgua on 03/12/2021.
+//
+
+import Foundation
+@testable import SwiftCafe
+
+extension OptionGroup {
+    static func createItems() -> [OptionGroup] {
+        var groups = [OptionGroup]()
+        for num in 1...Int.random(in: 2...4) {
+            var group = OptionGroup(name: "Option group \(num)", options: [Option]())
+            for index in 1...Int.random(in: 2...4) {
+                group.options.append(Option(name: "Option \(index)"))
+            }
+            groups.append(group)
+        }
+        return groups
+    }
+}
