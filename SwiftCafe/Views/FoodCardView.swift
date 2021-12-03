2bc01df4c24c03260b570b5c2d6f5f24a864789b
Ahmed Mgua <mguaahmed@gmail.com>
2021-12-03 07:47:18 +0300

    Fix removed strikethrough in price

--- SwiftCafe/Views/FoodCardView.swift
+++ SwiftCafe/Views/FoodCardView.swift
@@ -29,7 +29,6 @@ struct FoodCardView: View {
 
                         Text("\(food.price)")
                             .foregroundColor(.secondary)
-                            .strikethrough()
 
                     }
 
