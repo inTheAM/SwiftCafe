6d5c92cee519ed50038ce3563b6351f825fc7966
Ahmed Mgua <mguaahmed@gmail.com>
2021-12-03 14:37:53 +0300

    Add testFoodDetailsExist

--- SwiftCafeUITests/Tests/FoodDetailsViewUITests.swift
+++ SwiftCafeUITests/Tests/FoodDetailsViewUITests.swift
@@ -10,13 +10,28 @@ import XCTest
 
 final class FoodDetailsViewUITests: XCTestCase {
     let app = XCUIApplication()
-    
-//    MARK: - Setup
+
+// MARK: - Setup
     override func setUpWithError() throws {
         app.launchInTestEnvironment()
+        let foodCard = app.buttons["Food 1"].firstMatch
+        foodCard.tap()
     }
-    
-//    MARK: - Teardown
+
+// MARK: - Tests
+    func testFoodDetailsExist() throws {
+        let sheet = app.sheets["Food details"]
+        let foodName = sheet.staticTexts["Food 1"]
+        let foodPrice = sheet.staticTexts["$12"]
+        let foodDetails = sheet.staticTexts["Food details"]
+
+        XCTAssert(sheet.exists)
+        XCTAssert(foodName.exists)
+        XCTAssert(foodPrice.exists)
+        XCTAssert(foodDetails.exists)
+    }
+
+// MARK: - Teardown
     override func tearDownWithError() throws {
         app.terminate()
     }
