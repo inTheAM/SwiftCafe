ee38ce0f8e2cd1700258f4c4abe92a59657c1858
Ahmed Mgua <mguaahmed@gmail.com>
2021-12-08 16:51:54 +0300

    Refactored FoodDetailsView

---
+++ SwiftCafe/Views/FoodDetailsFooterView.swift
@@ -0,0 +1,67 @@
+//
+//  FoodDetailsFooterView.swift
+//  SwiftCafe
+//
+//  Created by Ahmed Mgua on 08/12/2021.
+//
+
+import SwiftUI
+
+struct FoodDetailsFooterView: View {
+    @ObservedObject var viewModel: FoodDetailsViewModel
+    @EnvironmentObject var cart: Cart
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Summary: \(viewModel.selectionSummary)")
+                    .bold()
+                    .foregroundColor(.primary)
+
+                Spacer()
+            }
+            .padding(.horizontal)
+            .padding(.vertical, 8)
+            .overlayDivider(.bottom)
+
+            HStack {
+                Text("Total: \(viewModel.totalPrice)")
+                    .bold()
+                    .accessibilityIdentifier("Total price")
+
+                Spacer()
+
+                Button {
+                    cart.contains(viewModel.food)  ?
+                    cart.remove(viewModel.food) :
+                    cart.add(viewModel.food, quantity: viewModel.quantity)
+
+                } label: {
+                    Text(cart.contains(viewModel.food) ?   "Remove from bag"  : "Add to bag")
+                        .font(.subheadline)
+                        .bold()
+                        .foregroundColor(
+                            cart.contains(viewModel.food) ?
+                            Color.white :
+                                Color(UIColor.systemBackground)
+                        )
+                        .buttonStyle(PlainButtonStyle())
+                        .padding(10)
+                        .background(cart.contains(viewModel.food) ? Color.red : Color.primary)
+                        .cornerRadius(20)
+                }
+                .accessibilityIdentifier("Add to cart")
+                .disabled(cart.isModifying)
+            }
+            .padding()
+        }
+//        .background(Color.blue.opacity(0.2).ignoresSafeArea())
+    }
+}
+
+struct FoodDetailsFooterView_Previews: PreviewProvider {
+    @ObservedObject static var cart = Cart()
+    static var previews: some View {
+        FoodDetailsFooterView(viewModel: FoodDetailsViewModel(food: Food.createItems()[0]))
+            .environmentObject(cart)
+    }
+}
--- SwiftCafe/Views/FoodDetailsView.swift
+++ SwiftCafe/Views/FoodDetailsView.swift
@@ -37,50 +37,8 @@ struct FoodDetailsView: View {
             .padding(.horizontal)
             .padding(.vertical, 8)
             .overlayDivider(.bottom)
-            VStack {
-                HStack {
-                    Text("Summary: \(viewModel.selectionSummary)")
-                        .bold()
-                        .foregroundColor(.primary)
-
-                    Spacer()
-                }
-                .padding(.horizontal)
-                .padding(.vertical, 8)
-                .overlayDivider(.bottom)
-
-                HStack {
-                    Text("Total: \(viewModel.totalPrice)")
-                        .bold()
-                        .accessibilityIdentifier("Total price")
-
-                    Spacer()
-
-                    Button {
-                        cart.contains(viewModel.food)  ?
-                        cart.remove(viewModel.food) :
-                        cart.add(viewModel.food, quantity: viewModel.quantity)
-
-                    } label: {
-                        Text(cart.contains(viewModel.food) ?   "Remove from bag"  : "Add to bag")
-                            .font(.subheadline)
-                            .bold()
-                            .foregroundColor(
-                                cart.contains(viewModel.food) ?
-                                Color.white :
-                                Color(UIColor.systemBackground)
-                            )
-                            .buttonStyle(PlainButtonStyle())
-                            .padding(10)
-                            .background(cart.contains(viewModel.food) ? Color.red : Color.primary)
-                            .cornerRadius(20)
-                    }
-                    .accessibilityIdentifier("Add to cart")
-                    .disabled(cart.isModifying)
-                }
-                .padding()
-            }
-            .background(Color.green.opacity(0.2).ignoresSafeArea())
+            
+            FoodDetailsFooterView(viewModel: viewModel)
         }
     }
 }
