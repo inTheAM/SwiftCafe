2a06d1be9782769911304349e6feaa1cb62da880
Ahmed Mgua <mguaahmed@gmail.com>
2021-12-15 16:19:51 +0300

    Add documentation to API

--- SwiftCafe/APIServices/API.swift
+++ SwiftCafe/APIServices/API.swift
@@ -7,7 +7,53 @@
 
 import Foundation
 
-struct API {
+/// The endpoints for the api resources.
+enum API {
+    
+    /// The base URL for all API requests.
     static let baseURL  =   URL(string: "http://127.0.0.1:8090/api/")
-
+    
+    
+    /// A collection of endpoints for User activity.
+    enum User: String {
+        
+        /// The endpoint for checking email availability.
+        case checkEmail = "users/checkemail/"
+        
+        /// The endpoint for signing up a user.
+        case signUp = "users/signup/"
+        
+        /// The endpoint for signing in a user.
+        case signIn = "users/signin/"
+        
+        /// The endpoint for signing out a user.
+        case signOut = "users/signout/"
+    }
+    
+    /// A collection of endpoints for fetching menu data.
+    enum Menu: String {
+        
+        /// The endpoint for fetching the menu sections.
+        case fetchMenu = "menu/"
+    }
+    
+    /// A collection of endpoints for Cart operations.
+    enum Cart: String {
+        
+        /// The endpoint for fetching the contents of the user's cart.
+        case fetchContents = "carts/contents/"
+        
+        /// The endpoint for adding items to the user's cart.
+        case addItem = "carts/add/"
+        
+        /// The endpoint for removing items from the user's cart.
+        case removeItem = "carts/remove/"
+    }
+    
+    /// A collection of endpoints for fetching Food details.
+    enum Food: String {
+        
+        /// The endpoint for fetching the options for the food.
+        case fetchOptions = "options/"
+    }
 }
