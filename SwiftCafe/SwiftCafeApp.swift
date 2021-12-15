ab0f29d185404ea93ce402e5e82790cc56464a6a
Ahmed Mgua <mguaahmed@gmail.com>
2021-12-15 22:18:39 +0300

    Add documentation

--- SwiftCafe/SwiftCafeApp.swift
+++ SwiftCafe/SwiftCafeApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 /// #The entry point for the application.
 struct SwiftCafeApp: App {
 
-    ///
+    /// The state that represents whether the user is signed in or not.
+    /// A present value for the token means that the user is signed in.
+    /// A value of nil for the token indicates that the user's session expired.
     @State private var isLoggedIn = AuthServiceFactory.create().token != nil
     var body: some Scene {
         WindowGroup {
