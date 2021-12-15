0280ed3a4e97a15b0be012d0cc817d76f5f190c9
Ahmed Mgua <mguaahmed@gmail.com>
2021-12-15 18:05:17 +0300

    Add LoadingState

---
+++ SwiftCafe/Utility/LoadingState.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingState.swift
+//  SwiftCafe
+//
+//  Created by Ahmed Mgua on 15/12/2021.
+//
+
+import Foundation
+
+/// A type representing the loading state of an asset
+enum LoadingState {
+    
+    /// indicates that loading is in progress.
+    case loading
+    
+    /// Indicates that loading is finished.
+    case loaded
+}
