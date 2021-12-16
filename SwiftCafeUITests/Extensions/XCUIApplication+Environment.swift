e55400b6cecaf4943e73dd9e43000e39e979596f
Ahmed Mgua <mguaahmed@gmail.com>
2021-12-16 08:23:42 +0300

    Add documentation

--- SwiftCafeUITests/Extensions/XCUIApplication+Environment.swift
+++ SwiftCafeUITests/Extensions/XCUIApplication+Environment.swift
@@ -8,6 +8,9 @@
 import XCTest
 
 extension XCUIApplication {
+    /// Launches the application in a test environment.
+    /// - Parameter isSignup: A boolean value indicating whether
+    ///                       the test involves sign-up or sign-in.
     func launchInTestEnvironment(isSignup: Bool = false) {
         if isSignup {
             self.launchEnvironment = ["ENV": "TEST_SIGNUP"]
