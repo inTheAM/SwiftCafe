9858a594c0f8af2a4a21d2e2c789025ef6bf4f39
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2022-11-28 19:29:20 +0100

    Added comments

--- SwiftUIDataFlow/SwiftUIDataFlow/ObservableObjects/ParentObservableObject.swift
+++ SwiftUIDataFlow/SwiftUIDataFlow/ObservableObjects/ParentObservableObject.swift
@@ -9,6 +9,9 @@ import Foundation
 
 class ParentObservableObject: ObservableObject {
     private let messages: [String] = ["Hello", "Ciao", "Ola"]
+    
+    /// `@Published` creates an implicit Publisher that announces any changes made to the variable.
+    /// `@Published` can only be used in classes, not structs.
     @Published var message: String = "Hi"
     
     func updateMessage() {
