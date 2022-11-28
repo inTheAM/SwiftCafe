375e07871b2badf5c47ea0f0d4630cf599c19bed
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2022-11-28 15:18:46 +0100

    Added State variables

---
+++ SwiftUIDataFlow/SwiftUIDataFlow/StateVariables/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  SwiftUIDataFlow
+//
+//  Created by Ahmed Mgua on 28/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    /// Use `@State` when you want to declare variables that may be changed and are being shown in the view or in a child view.
+    /// Whenever the value of a `State` changes, the `body` of the view will be computed again and the updates become visible on screen.
+    @State private var message: String = "Hi"
+    
+    /// An array of sample messages to display.
+    let messages: [String] = ["Hello", "Ciao", "Ola"]
+    
+    /// If the value will never change, use a `let` to declare it:
+    ///
+    ///     let message: String = "Hello"
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            
+            // The current message being displayed.
+            Text(message)
+                .padding()
+            
+            Spacer()
+            
+            // Sets a random message to display.
+            Button("Set message") {
+                message = messages.randomElement()!
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
