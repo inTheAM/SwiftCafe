98c62284b775a16084e350743e1d1e815b1de9e3
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2022-11-28 14:51:54 +0100

    Created Data flow project

---
+++ SwiftUIDataFlow/SwiftUIDataFlow/SwiftUIDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDataFlowApp.swift
+//  SwiftUIDataFlow
+//
+//  Created by Ahmed Mgua on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
